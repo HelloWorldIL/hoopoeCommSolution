47e3dd1b3e34c5c9e78f3668cb5d62a4bfa0e66b
HelloWorldIL <idobronfeld@gmail.com>
2019-03-16 20:18:19 +0200

    fixed oppler calculation

--- scripts/newDoppler.py
+++ scripts/newDoppler.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 from skyfield.api import Topos, load, utc
-from datetime import date, time, datetime
+from skyfield.sgp4lib import EarthSatellite
+import socket
+import time
+import numpy as np
+from datetime import datetime, timedelta
 from argparse import ArgumentParser
 from configparser import ConfigParser
 import os
+from telnetlib import Telnet
+
 
 args = ArgumentParser()
 args.add_argument("--config", type=str, help="Configuration file path", default=os.environ['HOME']+"/.hslCommSolution/config.ini")
@@ -11,12 +17,33 @@ args.add_argument("--config", type=str, help="Configuration file path", default=
 config = ConfigParser()
 config.read(args.parse_args().config)
 
-
 sat = load.tle(config['TLE']['url'])[config['TLE']['sat']]
-station = Topos(config['Ground Station']['lat'], config['Ground Station']['lon'])
+station = Topos(config['Ground Station']['lat'], config['Ground Station']['lon'], elevation_m=int(config['Ground Station']['alt']))
 
-t = load.timescale().utc(datetime.now().replace(tzinfo=utc))
+F0 = int(config['Doppler']['freq'])
 
-diff = (sat-station).at(t)
+def dopplerCalc(sat, station, t, F0):
+    t1 = load.timescale().utc(t.utc_datetime()+timedelta(seconds=1))
+    C = 299792458
 
-# TODO Add doppler and rotator correction
+    diff = (sat - station).at(t)
+    diff1 = (sat - station).at(t1)
+    velocity_magnitude = diff.distance().km
+    velocity_magnitude1 = diff1.distance().km
+    change = (velocity_magnitude-velocity_magnitude1)*1000
+    return int((F0 * (C + change) / (C)))
+
+
+def sendDoppler(tn ,doppler):
+    try:
+        tn.write(("F " + str(doppler)).encode("ascii"))
+    except socket.error:
+        print("Connection refused, is the host running?")
+
+tn = Telnet("localhost", config['Doppler']['port'])
+while True:
+    t = load.timescale().utc(datetime.utcnow().replace(tzinfo=utc))
+    doppler = dopplerCalc(sat, station, t, F0)
+    print(doppler)
+    sendDoppler(tn, doppler)
+    time.sleep(0.1)
