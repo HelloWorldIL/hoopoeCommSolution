e1ca11e65d5c2633951a80852ef7d1c2e398fedc
HelloWorldIL <idobronfeld@gmail.com>
2019-03-23 11:38:01 -0700

    added install script and rotator control

---
+++ scripts/install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+DEPENDENCIES="python-pip python3-pip git cmake build-essential g++ python-mako python-six libogg-dev libvorbis-dev libpng-dev libpng++-dev"
+
+sudo apt update
+sudo apt install -y $DEPENDENCIES
+pip install git+https://github.com/gnuradio/pybombs.git
+
+pip3 install numpy
+pip3 install skyfield
+
+sudo apt install -y gpredict
+
+# pybombs setup in gnuradio installation
+pybombs auto-config
+pybombs recipes add-defaults
+pybombs prefix init ~/prefix -a main -R gnuradio-default
+pybombs install gr-satellites
+pybombs install gr-bruninga
+pybombs install gr-limesdr
+pybombs install gr-osmosdr
+pybombs install gqrx
+
+# gr-satnogs install
+cd ~/prefix/src
+git clone https://gitlab.com/librespacefoundation/satnogs/gr-satnogs.git
+cd gr-satnogs
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=~/prefix ..
+make
+sudo make install
--- scripts/newDoppler.py
+++ scripts/newDoppler.py
@@ -9,6 +9,7 @@ from argparse import ArgumentParser
 from configparser import ConfigParser
 import os
 from telnetlib import Telnet
+import subprocess
 
 
 args = ArgumentParser()
@@ -18,11 +19,26 @@ args.add_argument("--config", type=str, help="Configuration file path",
 config = ConfigParser()
 config.read(args.parse_args().config)
 
-sat = load.tle(config['TLE']['url'])[config['TLE']['sat']]
-station = Topos(config['Ground Station']['lat'], config['Ground Station']
-                ['lon'], elevation_m=int(config['Ground Station']['alt']))
+# Station prams
+stationlat = config['Ground Station']['lat']
+stationlon = config['Ground Station']['lon']
+stationAlt = int(config['Ground Station']['alt'])
 
-F0 = int(config['Doppler']['freq'])
+# TLE and sat params
+tleURL = config['TLE']['url']
+satName = config['TLE']['sat']
+F0 = int(config['Doppler']['freq'])  # Center Frequency
+
+sat = load.tle(tleURL)[satName]
+station = Topos(stationlat, stationlon, elevation_m=stationAlt)
+
+# Network params
+dopplerPort = config['Doppler']['port']
+rotatorPort = config['Rotator']['port']
+
+# Rotator params
+rotatorModel = config['Rotator']['model']
+rotatorDevice = config['Rotator']['device']
 
 
 def dopplerCalc(sat, station, t, F0):
@@ -36,24 +52,66 @@ def dopplerCalc(sat, station, t, F0):
     change = (velocity_magnitude-velocity_magnitude1)*1000
     return int((F0 * (C + change) / (C)))
 
+
 def altAzCalc(sat, station, t):
     diff = (sat - station).at(t)
     return (diff.altaz())
+
 
 def sendDoppler(tn, doppler):
     try:
         tn.write(("F " + str(doppler)).encode("ascii"))
     except socket.error:
+        print("Send failed, is the host running?")
+
+
+def sendRotatorTCP(tn, az, el):
+    try:
+        tn.write(("P " + str(az) + ", " + str(az)).encode("ascii"))
+    except socket.error:
+        print("Send failed, is the host running?")
+
+
+def connectDoppler(host, port):
+    try:
+        return Telnet(host, port)
+    except socket.error:
         print("Connection refused, is the host running?")
 
 
-# tn = Telnet("localhost", config['Doppler']['port'])
-while True:
-    t = load.timescale().utc(datetime.utcnow().replace(tzinfo=utc))
-    el, az, dist = altAzCalc(sat, station, t)
-    doppler = dopplerCalc(sat, station, t, F0)
-    print("Azimuth: " + str(int(az._degrees)))
-    print("Elevation: " + str(int(el.degrees)))
-    print("Frequency: " + str(doppler))
-    # sendDoppler(tn, doppler)
-    time.sleep(0.2)
+def connectRotator(model, device):
+    proc = subprocess.Popen(f'rotctl --model={model} --rot-file={device}', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc.communicate()
+    if proc.returncode != 0:
+        print("Error connecting to rotator")
+    return proc
+
+
+def main():
+    tn = connectDoppler("localhost", dopplerPort)
+    tnRot = connectDoppler("localhost", rotatorPort)
+    while True:
+        t = load.timescale().utc(datetime.utcnow().replace(tzinfo=utc))
+        el, az, dist = altAzCalc(sat, station, t)
+        doppler = dopplerCalc(sat, station, t, F0)
+        print("Azimuth: " + str(int(az._degrees)))
+        print("Elevation: " + str(int(el.degrees)))
+        print("Frequency: " + str(doppler))
+        sendDoppler(tn, doppler)
+        sendRotatorTCP(tnRot, int(az.degrees), int(el.degrees))
+        time.sleep(0.2)
+
+
+def sendRotator(proc, az, el):
+    toSend = f'P {az} {el}'
+    if int(el) > 0:
+        return proc.communicate(input=toSend.encode())
+    print("Satellite is not over the horizon!")
+
+
+print(f'rotctl --model={rotatorModel} --rot-file={rotatorDevice}')
+
+rotctlProc = connectRotator(rotatorModel, rotatorDevice)
+
+#out, err = sendRotator(rotctlProc, str(5), str(10))
+rotctlProc.kill()
