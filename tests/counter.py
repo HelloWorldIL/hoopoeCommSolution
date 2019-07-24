0142a7d9e39a245a504c94da40a1e38f22bb7755
HelloWorldIL <idobronfeld@gmail.com>
2019-07-24 08:49:22 +0300

    fixed counter test

--- tests/counter.py
+++ tests/counter.py
@@ -1,4 +1,5 @@
 import socket
+import collections
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -6,22 +7,49 @@ address = ('localhost', 52002)
 
 sock.connect(address)
 
+def kiss_decode(packet):
+    FEND = 0xc0
+    FESC = 0xdb
+    TFEND = 0xdc
+    TFESC = 0xdd
+
+    kiss = collections.deque()
+    kiss.extend(packet)
+    out = list()
+    while kiss:
+        c = kiss.popleft()
+        if c == FESC:
+            print('Excape Detected')
+            c = kiss.popleft()
+            if c == TFEND:
+                out.append(FEND)
+            elif c == TFESC:
+                out.append(FESC)
+        else:
+            out.append(c)
+    return bytearray(out)
+
 count = 0
 prev = -1
 missing = ""
 
-while prev < 254:
-    packt = sock.recv(4096)
-    if packt is not None and len(packt.hex()) > 20:
-        print(packt)
-        num = packt.hex()[-4]+packt.hex()[-3]
+while prev < 209:
+    packet = sock.recv(4096)
+    if packet is not None and len(packet.hex()) < 60:
+        # Kiss Decode
+        packet = kiss_decode(packet)
+        pakcet_hex = packet.hex()
+
+        # Get the Number in the data field
+        num = pakcet_hex[-4]+pakcet_hex[-3]
         num = int(num, 16)
         count+=1
         if prev+1 is not num:
-            for i in range(prev+1, num):
-                missing += str(i)
-                missing += ', '
-        prev = num
-        print(count)
-        print(num)
+            while prev is not num:
+                prev += 1
+                missing += str(prev) + ', '
+        print('Packet: ' + str(pakcet_hex))
+        print('Packet no. ' + str(count))
+        print('Packet data number: ' + str(num))
+
 print(missing)
