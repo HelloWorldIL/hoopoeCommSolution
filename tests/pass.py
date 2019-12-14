f4a8ae54a3db35e5abe5ee48464ebd53752f87d1
Ido Bronfeld <idobronfeld.gmail.com>
2019-12-14 08:36:59 -0800

    added pass script (print next pass)

--- tests/pass.py
+++ tests/pass.py
@@ -2,11 +2,10 @@ from hsl_comm import Predict
 from skyfield.api import load, Topos
 from datetime import datetime, timedelta
 
-sat = load.tle('https://celestrak.com/NORAD/elements/active.txt')['SHAONIAN XING']
+sat = load.tle('https://celestrak.com/NORAD/elements/active.txt')['DUCHIFAT-3']
 station = Topos(latitude='32.1150 N', longitude='34.7820 E', elevation_m=13)
 predict = Predict(sat, station)
-
-ts = load.timescale()
+ts = predict.timescale
 
 t = ts.now()
 t1 = ts.utc(t.utc_datetime()+timedelta(days=1))
@@ -15,10 +14,4 @@ tDateTime = t.utc_datetime()
 
 passes = predict.getNextPasses(t, t1)
 
-# start = boundaries[1][0]
-# end = boundaries[1][1]
-# times = predict.getNextPasses(start, end)
-
-for single_pass in passes:
-    if single_pass[2] > 1:
-        print(single_pass[2])
+print(passes[0])
